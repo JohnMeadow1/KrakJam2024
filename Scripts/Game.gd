850a39498443f23e40bd40e69a28da622f94dbb4
kobewi <kobewi4e@gmail.com>
2024-01-26 22:11:26 +0100

    powstaje gra

---
+++ Scripts/Game.gd
@@ -0,0 +1,5 @@
+extends Control
+
+@onready var strength: Range = %Strength
+@onready var intelligence: Range = %Intelligence
+@onready var shame: Range = %Shame
---
+++ Scripts/Gracz2.gd
@@ -0,0 +1,20 @@
+extends Control
+
+var paski: Array[Control]
+var selected_pasek: int
+
+func _ready() -> void:
+	paski.assign(get_tree().get_nodes_in_group("paski"))
+	update_wybrany_pasek()
+
+func _process(delta: float) -> void:
+	if selected_pasek < paski.size() - 1 and Input.is_action_just_pressed("pasek_next"):
+		selected_pasek += 1
+		update_wybrany_pasek()
+	elif selected_pasek > 0 and Input.is_action_just_pressed("pasek_poprzedni"):
+		selected_pasek -= 1
+		update_wybrany_pasek()
+
+func update_wybrany_pasek():
+	for i in paski.size():
+		paski[i].set_selected(i == selected_pasek)
