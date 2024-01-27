8524f108337d3b099e8e869d49e754ca5aac5499
kobewi <kobewi4e@gmail.com>
2024-01-27 02:38:50 +0100

    blokopierdarka

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -15,6 +15,7 @@ extends Control
 var wyborydata: Dictionary
 
 var scena: Node
+var blokuje: bool
 
 func _ready() -> void:
 	var t := TextDatabase.new()
@@ -75,6 +76,10 @@ func change_stat(stat: String, value: float):
 			whites.value += value
 
 func execute_pierd():
+	if blokuje:
+		# info?
+		return
+	
 	var pierd := Pierd.new()
 	pierd.volume = cukiers.pick - whites.pick
 	pierd.stink = fats.pick + whites.pick
@@ -84,7 +89,7 @@ func execute_pierd():
 	fats.value -= fats.pick * 0.1
 	whites.value -= whites.pick * 0.1
 	
-	var gruzarka: float = cukiers.value + fats.value + whites.value
+	var gruzarka: float = cukiers.pick + fats.pick + whites.pick
 	pierd.with_gruz = gruzarka > 70 and gruzarka < 110
 	
 	name_pierd(pierd)
--- Scripts/Gracz1.gd
+++ Scripts/Gracz1.gd
@@ -1,1 +1,19 @@
 extends Control
+
+@onready var wstrzymator: TextureProgressBar = %Wstrzymator
+
+var zaparcie: bool
+
+func _process(delta: float) -> void:
+	if not zaparcie and Input.is_action_pressed("blokuj_pierd"):
+		owner.blokuje = true
+		wstrzymator.value += 30 * delta
+		if is_equal_approx(wstrzymator.ratio, 1.0):
+			wstrzymator.modulate = Color.RED
+			zaparcie = true
+	else:
+		owner.blokuje = false
+		wstrzymator.value -= 10 * delta
+		if zaparcie and wstrzymator.value < 80:
+			zaparcie = false
+			wstrzymator.modulate = Color.WHITE
