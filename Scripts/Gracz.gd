672dcaec05d7db4ac1d0067aaa314f12cdff414f
kobewi <kobewi4e@gmail.com>
2024-01-27 16:03:12 +0100

    gradż

--- Scripts/Gracz.gd
+++ Scripts/Gracz.gd
@@ -2,14 +2,29 @@ extends CharacterBody2D
 
 @onready var how_to_play: Label = $Label
 @onready var how_to_devour: Label = $Label2
+@onready var nanimator: AnimationPlayer = $AnimationPlayer
+@onready var pragnienieniemaszansz_2d: Sprite2D = $Sprite2D
 
 var jadalne
 var praktik
+
+var chapczy: bool
 
 func _physics_process(delta: float) -> void:
 	var move := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = move * 300
 	move_and_slide()
+	
+	if not chapczy:
+		if velocity.is_zero_approx():
+			nanimator.play(&"Stanie")
+		else:
+			nanimator.play(&"Chodzenie")
+	
+	if velocity.x > 0:
+		pragnienieniemaszansz_2d.scale = Vector2(1, 1)
+	else:
+		pragnienieniemaszansz_2d.scale = Vector2(-1, 1)
 	
 	if praktik:
 		how_to_play.show()
@@ -26,5 +41,10 @@ func _physics_process(delta: float) -> void:
 		praktik.practice()
 
 	if is_jadalne and Input.is_action_just_pressed("żryj"):
-		$AnimationPlayer.play(&"Chaparka")
 		jadalne.zjedz()
+		
+		if not chapczy:
+			chapczy = true
+			nanimator.play(&"Chaparka")
+			await nanimator.animation_finished
+			chapczy = false
