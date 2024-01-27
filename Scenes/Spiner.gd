9836168bad4c767f732f457b5639cf33bb51cb29
John Meadow <armitage3@gazeta.pl>
2024-01-27 17:23:05 +0100

    Jeszcze lepsza implementacja sfx_spinner

--- Scenes/Spiner.gd
+++ Scenes/Spiner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var texture:Texture
+@export var lastvalveposition: float
 
 var is_pressed := false
 var previous_mouse_position := Vector2.ZERO
@@ -16,6 +17,7 @@ func _ready():
 	button.pivot_offset = texture.get_size() * 0.5
 	button.position = -button.pivot_offset
 	set_physics_process(false)
+	$AudioStreamPlayer2D.stream = spinnersound
 
 func _physics_process(delta):
 	var angle_difference = atan2(previous_mouse_position.cross(get_local_mouse_position()), previous_mouse_position.dot(get_local_mouse_position()) )
@@ -24,27 +26,26 @@ func _physics_process(delta):
 	value = clamp(value, 0.0, 100.0)
 	button.rotation = value / 2.0
 	updated.emit()
-
+	if value == 0.0:
+		$AudioStreamPlayer2D.stream_paused = true
+	if value == 100.0:
+		$AudioStreamPlayer2D.stream_paused = true
+	if value == clamp(value, 0.1, 99.9):
+		$AudioStreamPlayer2D.stream_paused = false
 
 func _on_button_down():
 	set_physics_process(true)
 	previous_mouse_position = get_local_mouse_position()
 	is_pressed = true
+	$AudioStreamPlayer2D.seek(lastvalveposition)
 	$AudioStreamPlayer2D.stream = spinnersound
-	$AudioStreamPlayer2D.play()
-	
-
+	$AudioStreamPlayer2D.play(lastvalveposition)
 
 func _on_button_up():
 	set_physics_process(false)
 	is_pressed = false
 	$AudioStreamPlayer2D.stream = spinnersound
 	$AudioStreamPlayer2D.stop()
+	
 
 
-func _on_audio_stream_player_2d_finished():
-	pass # Replace with function body.
-
-
-func _on_audio_stream_player_2d_child_entered_tree(node):
-	pass # Replace with function body.
