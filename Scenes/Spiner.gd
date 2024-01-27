e18a253fcab00627cf14c71cf3c73d722c68b969
kobewi <kobewi4e@gmail.com>
2024-01-27 18:46:02 +0100

    idealne kurki 🐔

--- Scenes/Spiner.gd
+++ Scenes/Spiner.gd
@@ -5,9 +5,8 @@ extends Node2D
 
 var is_pressed := false
 var previous_mouse_position := Vector2.ZERO
-var value = 50.0
-
-var spinnersound = preload("res://Audio/sfx_spinner.wav")
+var value := 50.0
+var playing: float
 
 @onready var button = $Button
 signal updated
@@ -16,35 +15,32 @@ func _ready():
 	button.texture_normal = texture
 	button.pivot_offset = texture.get_size() * 0.5
 	button.position = -button.pivot_offset
+	get_tree().create_timer(0.2).timeout.connect(func(): $AudioStreamPlayer2D.stream_paused = true) # aka call_super_fking_deferred()
 	set_physics_process(false)
-	$AudioStreamPlayer2D.stream = spinnersound
 
 func _physics_process(delta):
 	var angle_difference = atan2(previous_mouse_position.cross(get_local_mouse_position()), previous_mouse_position.dot(get_local_mouse_position()) )
 	previous_mouse_position = get_local_mouse_position()
+	var prev := value
 	value += angle_difference *2.0
 	value = clamp(value, 0.0, 100.0)
 	button.rotation = value / 2.0
 	updated.emit()
-	if is_zero_approx(value) or is_equal_approx(value, 100.0):
-		$AudioStreamPlayer2D.stream_paused = true
-	else:
-		$AudioStreamPlayer2D.stream_paused = false
+	
+	if not is_equal_approx(value, prev):
+		playing = 0.2
+	
+	$AudioStreamPlayer2D.stream_paused = playing <= 0
+	playing -= delta
 
 func _on_button_down():
 	set_physics_process(true)
 	previous_mouse_position = get_local_mouse_position()
 	is_pressed = true
 	#$AudioStreamPlayer2D.seek(lastvalveposition)
-	$AudioStreamPlayer2D.stream = spinnersound
-	$AudioStreamPlayer2D.play(lastvalveposition)
-	$AudioStreamPlayer2D.stream_paused = false
  
 func _on_button_up():
 	set_physics_process(false)
 	is_pressed = false
-	$AudioStreamPlayer2D.stream = spinnersound
 	$AudioStreamPlayer2D.stream_paused = true
-	
-
-
+	playing = 0
