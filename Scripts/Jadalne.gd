24b9fc327fe9ba16f24d512c23ea3d488d78f7bf
kobewi <kobewi4e@gmail.com>
2024-01-27 10:12:29 +0100

    alternatywne zakończenie

--- Scripts/Jadalne.gd
+++ Scripts/Jadalne.gd
@@ -18,7 +18,8 @@ func _on_body_exited(body: Node2D) -> void:
 		body.jadalne = null
 
 func can_eat() -> bool:
-	return get_tree().current_scene.glood >= hunger_requirement
+	return get_tree().current_scene.glood.value >= hunger_requirement
 
 func zjedz():
+	get_tree().current_scene.glood.value -= color.r * 50 # TODO
 	queue_free()
