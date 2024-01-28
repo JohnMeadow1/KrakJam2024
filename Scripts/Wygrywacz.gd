638ba66bcefff254312e18751160b9ff39515308
kobewi <kobewi4e@gmail.com>
2024-01-28 12:41:16 +0100

    wygryw

---
+++ Scripts/Wygrywacz.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var scena: String
+
+func _ready() -> void:
+	if not OS.has_feature("editor"):
+		queue_free()
+
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_1):
+		owner.game.zrobione_sceny.append(scena)
+		owner.game.load_scena("DasRaum")
